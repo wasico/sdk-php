04037b973e3de5bcf871d98731d44dd13680b62f
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-05-21 22:52:31 -0500

    U

--- src/Drivers/V1.php
+++ src/Drivers/V1.php
@@ -4,8 +4,13 @@ namespace Wasi\SDK\Drivers;
 
 class V1 implements Driver
 {
+    private $id_company;
+    private $wasi_token;
+
     function __construct(array $params = [])
     {
-        var_dump($params);
+        if(!isset($params['id_company'])) {
+            throw new \Exception("id_company is required");
+        }
     }
 }
