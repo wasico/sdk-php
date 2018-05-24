8c33aea7587d3fb99421210d7ce5978fb3518bb3
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-05-23 21:26:23 -0500

    u

---
+++ src/Classes/Attribute.php
@@ -0,0 +1,16 @@
+<?php
+
+class Attribute
+{
+    const INTEGER = 0;
+    const STRING = 1;
+
+    private $name;
+    private $type;
+
+    public function __construct(string $name, int $type)
+    {
+        $this->name = $name;
+        $this->type = $type;
+    }
+}
