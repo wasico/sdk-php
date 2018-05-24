1ebb2a3d464cf238458dca9ad2a5a53e9f8e89d4
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-05-23 21:31:26 -0500

    u

--- src/Models/Property.php
+++ src/Models/Property.php
@@ -2,12 +2,15 @@
 
 namespace Wasi\SDK\Models;
 
+use Wasi\SDK\Classes\Attribute;
 
 class Property extends Model
 {
     public function standartAttributes()
     {
         return [
+            new Attribute('id_property', Attribute::INTEGER),
+            new Attribute('tittle', Attribute::STRING),
         ];
     }
 }
