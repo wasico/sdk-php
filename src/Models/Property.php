78a057d716f4d78a462ac61574c94cf712f3e54f
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-06-10 19:29:27 -0500

    u

--- src/Models/Property.php
+++ src/Models/Property.php
@@ -11,6 +11,7 @@ class Property extends Model
         return [
             'id_property' => new Attribute('id_property', Attribute::INTEGER),
             'id_company'  => new Attribute('id_company', Attribute::INTEGER),
+            'id_user'     => new Attribute('id_user', Attribute::INTEGER),
             'title'       => new Attribute('tittle', Attribute::STRING),
         ];
     }
