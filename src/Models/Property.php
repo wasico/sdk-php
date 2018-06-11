1dbfd61a9e397dd094fc048f5dbaef9bcd323f07
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-06-10 19:39:19 -0500

    u

--- src/Models/Property.php
+++ src/Models/Property.php
@@ -9,13 +9,14 @@ class Property extends Model
     public function standartAttributes()
     {
         return [
-            'id_property'  => new Attribute(Attribute::INTEGER),
-            'id_company'   => new Attribute(Attribute::INTEGER),
-            'id_user'      => new Attribute(Attribute::INTEGER),
-            'for_sale'     => new Attribute(Attribute::BOOLEAN),
-            'for_rent'     => new Attribute(Attribute::BOOLEAN),
-            'for_transfer' => new Attribute(Attribute::BOOLEAN),
-            'title'        => new Attribute(Attribute::STRING),
+            'id_property'      => new Attribute(Attribute::INTEGER),
+            'id_company'       => new Attribute(Attribute::INTEGER),
+            'id_user'          => new Attribute(Attribute::INTEGER),
+            'for_sale'         => new Attribute(Attribute::BOOLEAN),
+            'for_rent'         => new Attribute(Attribute::BOOLEAN),
+            'for_transfer'     => new Attribute(Attribute::BOOLEAN),
+            'id_property_type' => new Attribute(Attribute::INTEGER),
+            'title'            => new Attribute(Attribute::STRING),
         ];
     }
 }
