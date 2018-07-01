a75e1fd06768659faee3a607f84e2f4f91ed3e3e
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-07-01 16:46:02 -0500

    Update Customer attributes

--- src/Models/Customer.php
+++ src/Models/Customer.php
@@ -9,15 +9,20 @@ class Customer extends Model
     public function standartAttributes()
     {
         return [
-            'id_client'      => new Attribute(Attribute::INTEGER, false),
-            'id_user'        => new Attribute(Attribute::INTEGER),
-            'id_client_type' => new Attribute(Attribute::INTEGER),
-            'id_country'     => new Attribute(Attribute::INTEGER),
-            'country_label'  => new Attribute(Attribute::STRING, false),
-            'id_region'      => new Attribute(Attribute::INTEGER),
-            'region_label'   => new Attribute(Attribute::STRING, false),
-            'id_city'        => new Attribute(Attribute::INTEGER),
-            'city_label'     => new Attribute(Attribute::STRING, false),
+            'id_client'        => new Attribute(Attribute::INTEGER, false),
+            'id_user'          => new Attribute(Attribute::INTEGER),
+            'id_client_type'   => new Attribute(Attribute::INTEGER),
+            'id_country'       => new Attribute(Attribute::INTEGER),
+            'country_label'    => new Attribute(Attribute::STRING, false),
+            'id_region'        => new Attribute(Attribute::INTEGER),
+            'region_label'     => new Attribute(Attribute::STRING, false),
+            'id_city'          => new Attribute(Attribute::INTEGER),
+            'city_label'       => new Attribute(Attribute::STRING, false),
+            'id_client_status' => new Attribute(Attribute::INTEGER),
+            'first_name'       => new Attribute(Attribute::STRING),
+            'last_name'        => new Attribute(Attribute::STRING),
+            'birthday'         => new Attribute(Attribute::STRING),
+            'identification'   => new Attribute(Attribute::STRING),
         ];
     }
 }
