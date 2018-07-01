75460a5cf78bfc5f994e35c0f96bcc6d63fe20ee
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-07-01 14:58:13 -0500

    Update attributes to Customer

--- src/Models/Customer.php
+++ src/Models/Customer.php
@@ -12,6 +12,12 @@ class Customer extends Model
             'id_client'      => new Attribute(Attribute::INTEGER, false),
             'id_user'        => new Attribute(Attribute::INTEGER),
             'id_client_type' => new Attribute(Attribute::INTEGER),
+            'id_country'     => new Attribute(Attribute::INTEGER),
+            'country_label'  => new Attribute(Attribute::STRING, false),
+            'id_region'      => new Attribute(Attribute::INTEGER),
+            'region_label'   => new Attribute(Attribute::STRING, false),
+            'id_city'        => new Attribute(Attribute::INTEGER),
+            'city_label'     => new Attribute(Attribute::STRING, false),
         ];
     }
 }
