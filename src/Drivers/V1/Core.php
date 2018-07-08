d92dc184bae3db8d4c21584e32be4d840bf85f07
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-07-07 20:26:25 -0500

    u

--- src/Drivers/V1/Core.php
+++ src/Drivers/V1/Core.php
@@ -6,6 +6,7 @@ use Wasi\SDK\Drivers\Driver;
 use Wasi\SDK\Models\Customer;
 use Wasi\SDK\Models\CustomerType;
 use Wasi\SDK\Models\Model;
+use Wasi\SDK\Models\Portal;
 use Wasi\SDK\Models\Property;
 use Wasi\SDK\Models\User;
 
@@ -99,6 +100,9 @@ class Core implements Driver
                 break;
             case CustomerType::class:
                 $url = 'client-type/all';
+                break;
+            case Portal::class:
+                $url = 'portal/all';
                 break;
             default:
                 $url = '';
