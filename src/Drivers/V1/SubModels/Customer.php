eef1cbbd9a206350414cbc03788abfed83b891cb
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-07-21 18:05:01 -0500

    Add Customer SubModel

---
+++ src/Drivers/V1/SubModels/Customer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Wasi\SDK\Drivers\V1\SubModels;
+
+class Customer implements SubModel
+{
+
+    public static function urlFind(): string
+    {
+        return 'client/get/';
+    }
+
+    public static function urlGet(): string
+    {
+        return 'client/search';
+    }
+}
