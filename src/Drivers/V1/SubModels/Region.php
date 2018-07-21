e399a68fe2b8eaa89045b88e9d2cbde67821091a
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-07-21 18:46:32 -0500

    add Region SubModel

---
+++ src/Drivers/V1/SubModels/Region.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Wasi\SDK\Drivers\V1\SubModels;
+
+class Region implements SubModel
+{
+
+    public static function urlFind(): string
+    {
+        return 'location/region/';
+    }
+
+    public static function urlGet(): string
+    {
+        return null;
+    }
+}
