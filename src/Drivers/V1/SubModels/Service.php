40e46731e8306ce57eae1c3465071f1221d50634
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-07-21 18:37:57 -0500

    add Service SubModel

---
+++ src/Drivers/V1/SubModels/Service.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Wasi\SDK\Drivers\V1\SubModels;
+
+class Service implements SubModel
+{
+
+    public static function urlFind(): string
+    {
+        return 'service/get/';
+    }
+
+    public static function urlGet(): string
+    {
+        return 'service/search';
+    }
+}
