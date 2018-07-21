9e304ce14e26a3d41ac96adcf025c0d8b3094783
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-07-20 19:05:41 -0500

    u

---
+++ src/Models/Country.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Wasi\SDK\Models;
+
+use Wasi\SDK\Classes\Attribute;
+
+class Country extends Model
+{
+    public function standartAttributes()
+    {
+        return [
+            'id_country' => new Attribute(Attribute::INTEGER, false),
+            'name' => new Attribute(Attribute::STRING, false),
+            'iso' => new Attribute(Attribute::STRING, false),
+        ];
+    }
+}
