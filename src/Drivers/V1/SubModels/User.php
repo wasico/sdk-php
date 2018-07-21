e4fdadfcceb438a64bfbfb27a63b0d4403d2e322
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-07-21 18:31:42 -0500

    add User SubModel

---
+++ src/Drivers/V1/SubModels/User.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Wasi\SDK\Drivers\V1\SubModels;
+
+class User implements SubModel
+{
+
+    public static function urlFind(): string
+    {
+        return 'user/get/';
+    }
+
+    public static function urlGet(): string
+    {
+        return 'user/all-users';
+    }
+}
