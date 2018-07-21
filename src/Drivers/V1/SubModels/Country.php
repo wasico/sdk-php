f4cef84692425b8d5a2d06143f1df74acc786ec9
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-07-21 17:56:05 -0500

    add Country SubClass

---
+++ src/Drivers/V1/SubModels/Country.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Wasi\SDK\Drivers\V1\SubModels;
+
+class Country implements SubModel
+{
+
+    public static function urlFind(): string
+    {
+        return 'location/country/';
+    }
+
+    public static function urlGet(): string
+    {
+        return 'location/all-countries';
+    }
+}
