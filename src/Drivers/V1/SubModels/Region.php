c192387b43c80c844302e0acd56bd3dfe695cf9a
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-07-21 19:02:27 -0500

    Update Region SubModel

--- src/Drivers/V1/SubModels/Region.php
+++ src/Drivers/V1/SubModels/Region.php
@@ -5,12 +5,12 @@ namespace Wasi\SDK\Drivers\V1\SubModels;
 class Region implements SubModel
 {
 
-    public static function urlFind(): string
+    public static function urlFind(): ? string
     {
         return 'location/region/';
     }
 
-    public static function urlGet(): string
+    public static function urlGet(): ? string
     {
         return null;
     }
