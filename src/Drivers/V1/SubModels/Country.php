256d492915887294e3c434b2b7a07138c5c8d91d
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-07-21 19:04:29 -0500

    Update Country SubModel

--- src/Drivers/V1/SubModels/Country.php
+++ src/Drivers/V1/SubModels/Country.php
@@ -5,12 +5,12 @@ namespace Wasi\SDK\Drivers\V1\SubModels;
 class Country implements SubModel
 {
 
-    public static function urlFind(): string
+    public static function urlFind(): ? string
     {
         return 'location/country/';
     }
 
-    public static function urlGet(): string
+    public static function urlGet(): ? string
     {
         return 'location/all-countries';
     }
