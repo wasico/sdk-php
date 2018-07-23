cc8072818a6d83be5283342a5326c23918d1c303
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-07-22 20:33:06 -0500

    add Zone Model

---
+++ src/Models/Zone.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Wasi\SDK\Models;
+
+use Wasi\SDK\Classes\Attribute;
+
+class Zone extends Model
+{
+    public function standartAttributes()
+    {
+        return [
+            'id_zone' => new Attribute(Attribute::INTEGER, false),
+            'name' => new Attribute(Attribute::STRING, false),
+            'id_city' => new Attribute(Attribute::INTEGER, false),
+            'id_location' => new Attribute(Attribute::INTEGER, false),
+            'owner' => new Attribute(Attribute::STRING, false),
+        ];
+    }
+}
