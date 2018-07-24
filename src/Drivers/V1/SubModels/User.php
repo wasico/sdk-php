89c02970d1995afeec35d7fe639c0145fae2c977
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-07-24 18:35:48 -0500

    Update User SubModel

--- src/Drivers/V1/SubModels/User.php
+++ src/Drivers/V1/SubModels/User.php
@@ -4,7 +4,7 @@ namespace Wasi\SDK\Drivers\V1\SubModels;
 
 use Wasi\SDK\Models\Model;
 
-class User extends SubModel
+class User implements SubModel
 {
 
     public static function urlFind(Model $model): ? string
