024578962f9fb63da14685ea143a8396a3f45b3b
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-07-23 19:56:13 -0500

    Update City Model

--- src/Models/City.php
+++ src/Models/City.php
@@ -14,4 +14,19 @@ class City extends Model
             'id_region' => new Attribute(Attribute::INTEGER, false),
         ];
     }
+
+    public function region()
+    {
+        return Region::find($this->attributes['id_region']);
+    }
+
+    public function locations()
+    {
+        return Location::where('id_city', $this->attributes['id_city'])->get();
+    }
+
+    public function zones()
+    {
+        return Zone::where('id_city', $this->attributes['id_city'])->get();
+    }
 }
