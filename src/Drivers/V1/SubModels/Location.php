94da123cc24d3b38837f8b66211d21b043c5e0b5
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-07-23 19:53:31 -0500

    Update Location SubModel

--- src/Drivers/V1/SubModels/Location.php
+++ src/Drivers/V1/SubModels/Location.php
@@ -2,16 +2,21 @@
 
 namespace Wasi\SDK\Drivers\V1\SubModels;
 
+use Wasi\SDK\Models\Model;
+
 class Location implements SubModel
 {
 
-    public static function urlFind(): ? string
+    public static function urlFind(Model $model): ? string
     {
         return 'location/location/';
     }
 
-    public static function urlGet(): ? string
+    public static function urlGet(Model $model): ? string
     {
-        return null;
+        $whereArray = $model->getWhereArray();
+        if(!isset($whereArray['id_city']))
+            throw new WhereIsRequireException("Where with id_city is required");
+        return 'location/locations-from-city/'.$whereArray['id_city'];
     }
 }
