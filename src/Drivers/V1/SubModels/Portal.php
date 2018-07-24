29f892c153b2f58deece735cf3d0966eaaae6b20
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-07-24 18:45:31 -0500

    Update Portal SubModel

--- src/Drivers/V1/SubModels/Portal.php
+++ src/Drivers/V1/SubModels/Portal.php
@@ -4,7 +4,7 @@ namespace Wasi\SDK\Drivers\V1\SubModels;
 
 use Wasi\SDK\Models\Model;
 
-class Portal extends SubModel
+class Portal implements SubModel
 {
 
     public static function urlFind(Model $model): ? string
