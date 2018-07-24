8b1dd85d7f118f489f2924b02e63b3e5f9af0d24
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-07-23 19:55:30 -0500

    Update Zone SubModel

--- src/Drivers/V1/SubModels/Zone.php
+++ src/Drivers/V1/SubModels/Zone.php
@@ -2,16 +2,23 @@
 
 namespace Wasi\SDK\Drivers\V1\SubModels;
 
+use Wasi\SDK\Models\Model;
+
 class Zone implements SubModel
 {
 
-    public static function urlFind(): ? string
+    public static function urlFind(Model $model): ? string
     {
         return 'location/zone/';
     }
 
-    public static function urlGet(): ? string
+    public static function urlGet(Model $model): ? string
     {
-        return null;
+        $whereArray = $model->getWhereArray();
+        if(!isset($whereArray['id_city']) && !isset($whereArray['id_location']))
+            throw new WhereIsRequireException("Where with id_location or id_city is required");
+        if(isset($whereArray['id_location']))
+            return 'location/zones-from-location/'.$whereArray['id_location'];
+        return 'location/zones-from-city/'.$whereArray['id_city'];
     }
 }
