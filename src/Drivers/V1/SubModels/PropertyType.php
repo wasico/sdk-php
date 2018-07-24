8341479fc423d913fb920aca3dcabb11fcd60c4c
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-07-24 18:40:50 -0500

    Update PropertyType SubModel

--- src/Drivers/V1/SubModels/PropertyType.php
+++ src/Drivers/V1/SubModels/PropertyType.php
@@ -4,7 +4,7 @@ namespace Wasi\SDK\Drivers\V1\SubModels;
 
 use Wasi\SDK\Models\Model;
 
-class PropertyType extends SubModel
+class PropertyType implements SubModel
 {
 
     public static function urlFind(Model $model): ? string
