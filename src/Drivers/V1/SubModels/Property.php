58911f6ae075fa55cda63c6ec0d267e1722c55b6
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-07-23 19:53:57 -0500

    Update Property SubModel

--- src/Drivers/V1/SubModels/Property.php
+++ src/Drivers/V1/SubModels/Property.php
@@ -2,16 +2,23 @@
 
 namespace Wasi\SDK\Drivers\V1\SubModels;
 
+use Wasi\SDK\Models\Model;
+
 class Property implements SubModel
 {
 
-    public static function urlFind(): ? string
+    public static function urlFind(Model $model): ? string
     {
         return 'property/get/';
     }
 
-    public static function urlGet(): ? string
+    public static function urlGet(Model $model): ? string
     {
         return 'property/search';
+    }
+
+    public static function owner(Model $model)
+    {
+        return $url = 'property/owner/'.$model->id_property;//temp
     }
 }
