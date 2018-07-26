c06468f2ce0a3a8f474966a0f61d0fa69f5a781a
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-07-25 20:16:53 -0500

    Bugfix URL CustomerType SubModel

--- src/Drivers/V1/SubModels/CustomerType.php
+++ src/Drivers/V1/SubModels/CustomerType.php
@@ -14,6 +14,6 @@ class CustomerType extends SubModel
 
     public static function urlGet(Model $model): ? string
     {
-        return 'property-type/all';
+        return 'client-type/all';
     }
 }
