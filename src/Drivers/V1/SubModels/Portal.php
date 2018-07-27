1e0760c06311b964c145ffe0b833a771d75daac5
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-07-26 19:44:05 -0500

    Update Portal SubModel

--- src/Drivers/V1/SubModels/Portal.php
+++ src/Drivers/V1/SubModels/Portal.php
@@ -2,6 +2,7 @@
 
 namespace Wasi\SDK\Drivers\V1\SubModels;
 
+use Wasi\SDK\Drivers\V1\WrappedModel;
 use Wasi\SDK\Models\Model;
 
 class Portal extends SubModel
@@ -15,5 +16,10 @@ class Portal extends SubModel
     public static function urlGet(Model $model): ? string
     {
         return 'portal/all';
+    }
+
+    public static function property($class, $id_property)
+    {
+        return new WrappedModel(\Wasi\SDK\Models\Portal::class, "portal/property/$id_property");
     }
 }
