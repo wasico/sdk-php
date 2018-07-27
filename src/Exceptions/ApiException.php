0816d8c13885ca73ee11ffe369d2f138fb8f19ff
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-07-26 20:02:55 -0500

    Create ApiException

---
+++ src/Exceptions/ApiException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Wasi\SDK\Exceptions;
+
+class ApiException extends \Exception
+{
+    public function __construct(string $message = "", int $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
