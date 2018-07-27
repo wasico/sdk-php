3860532f62f686eeb1b6c173d2aaf436ef7a2cc0
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-07-27 01:19:29 -0500

    Create C class

---
+++ src/Classes/C.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Wasi\SDK\Classes;
+
+
+class C
+{
+    const RENT_TYPE_DAILY = 1;
+    const RENT_TYPE_WEEKLY = 2;
+    const RENT_TYPE_BIWEEKLY = 3;
+    const RENT_TYPE_MONTHLY = 4;
+}
