4084b71ca00cf1bd82c34d418c233f8c429a78c1
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-07-27 01:24:45 -0500

    Update C class

--- src/Classes/C.php
+++ src/Classes/C.php
@@ -9,4 +9,9 @@ class C
     const RENT_TYPE_WEEKLY = 2;
     const RENT_TYPE_BIWEEKLY = 3;
     const RENT_TYPE_MONTHLY = 4;
+
+    const STATUS_ON_PAGE_ACTIVE = 1;
+    const STATUS_ON_PAGE_INACTIVE = 2;
+    const STATUS_ON_PAGE_HIGHLIGHTED = 3;
+    const STATUS_ON_PAGE_DELETED = 4;
 }
