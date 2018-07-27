2e18070f175335534b430e76f331886fd151a74a
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-07-27 00:25:20 -0500

    Create Image Model

---
+++ src/Models/Image.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Wasi\SDK\Models;
+
+use Wasi\SDK\Classes\Attribute;
+
+class Image extends Model
+{
+    public function standartAttributes()
+    {
+        return [
+            'id' => new Attribute(Attribute::INTEGER, false),
+            'url' => new Attribute(Attribute::STRING, false),
+            'url_big' => new Attribute(Attribute::STRING, false),
+            'description' => new Attribute(Attribute::STRING, false),
+            'position' => new Attribute(Attribute::INTEGER, false),
+        ];
+    }
+}
