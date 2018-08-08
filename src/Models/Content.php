3bdc4410b5a536d67b672b3b4d44a8613ef0ae75
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2018-08-08 11:18:50 -0500

    Create Content Model

---
+++ src/Models/Content.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Wasi\SDK\Models;
+
+use Wasi\SDK\Classes\Attribute;
+
+class Content extends Model
+{
+    public function standartAttributes()
+    {
+        return [
+            'id_content' => new Attribute(Attribute::INTEGER, false),
+            'title' => new Attribute(Attribute::STRING, false),
+            'content' => new Attribute(Attribute::STRING, false),
+            'link' => new Attribute(Attribute::STRING, false),
+            'delete' => new Attribute(Attribute::BOOLEAN, false),
+        ];
+    }
+}
